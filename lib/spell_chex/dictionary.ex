d65cec637deac49cdcb5398825bd9e085e999e9d
Byron Salty <byronsalty@gmail.com>
2024-01-07 22:45:58 -0600

    added nsfw words

--- lib/spell_chex/dictionary.ex
+++ lib/spell_chex/dictionary.ex
@@ -36,7 +36,14 @@ defmodule SpellChex.Dictionary do
 
   @impl true
   def init(_) do
-    all_words = FileReader.read_words_from_file("deps/spell_chex/priv/20k.txt")
+    path =
+      if File.exists?("deps/spell_chex/priv/20k.txt") do
+        "deps/spell_chex/priv/20k.txt"
+      else
+        "/app/lib/spell_chex-0.0.1/priv/20k.txt"
+      end
+
+    all_words = FileReader.read_words_from_file(path)
     {:ok, all_words}
   end
 
