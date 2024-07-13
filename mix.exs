2d345b5c8bd4e35ce7c756664796cd047e337f9f
Byron Salty <byronsalty@gmail.com>
2024-07-12 22:31:37 -0400

    bumped version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SpellChex.MixProject do
   def project do
     [
       app: :spell_chex,
-      version: "0.0.19",
+      version: "0.0.20",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
