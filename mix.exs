e474576dd8cc344b1192fc4c76c85e8d8aadf985
Byron Salty <byronsalty@gmail.com>
2025-02-19 23:46:12 -0600

    adding words

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SpellChex.MixProject do
   def project do
     [
       app: :spell_chex,
-      version: "0.0.23",
+      version: "0.0.24",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
